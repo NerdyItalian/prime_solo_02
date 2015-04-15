0e5a85cdd3a2ebdcd6a70ecda120774f154d01db
NerdyItalian <atava8384@gmail.com>
2015-04-15 17:37:25 -0500

    Initial Commit

--- solo_script.js
+++ solo_script.js
@@ -16,12 +16,14 @@ position = document.getElementById('content');
 //Loop the array, extracting each array and writing information to the DOM
 //Note that the information is not 'clean'
 for(var i = 0; i < array.length; i++){
-	array[i] = calculateSTI(array);
+	array[i] = calculateSTI(array[i]);
  	newEl = document.createElement('li');
 	newText = document.createTextNode(array[i]);
 	newEl.appendChild(newText);
 	position.appendChild(newEl);
 }
+
+
 
 function calculateSTI(array){
   var newArray = [];
@@ -37,12 +39,14 @@ function calculateSTI(array){
     bonus = 0.13;
   }
 
-  newArray[1] = bonus;
-  newArray[2] = baseSalary * (1.0 + bonus);
-  newArray[3] = baseSalary * bonus;
+  newArray[1] = " " + bonus;
+  newArray[2] = " " + Math.round(baseSalary * (1.0 + bonus));
+  newArray[3] = " " + baseSalary * bonus;
   console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
   return newArray;
+  console.log(newArray);
 }
+
 
 function getBaseSTI(reviewScore){
   var basePercent;
@@ -63,7 +67,7 @@ function getBaseSTI(reviewScore){
       basePercent = 0.10;
       break;
   }
-  return basePercent - 1;
+  return basePercent;
 }
 
 function getYearAdjustment(employeeNumber){
